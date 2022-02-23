dfd80551250fd4d2fe62d22f5a94c31f74500737
Verdant808 <rapp0660@uwlax.edu>
2022-02-23 17:13:26 -0600

    Interface beautification
    Adjust the placement of a few widgets.
    
    Image names are now taken from a predetermined  directory (path + 'datafile'), and stored as a list.

--- Window.py
+++ Window.py
@@ -14,15 +14,17 @@ root.columnconfigure(1, weight=1)
 root.rowconfigure(2, weight=1)
 
 # default image
-imagePath = os.getcwd() + '\\datafiles\\' + 'img00.jpg'
+path = os.getcwd() + os.path.sep + 'datafiles'
+imageNames = os.listdir(path)
+imagePath = path + os.path.sep + imageNames[2]
 imageOpen = Image.open(imagePath)
 imagePreview = ImageTk.PhotoImage(imageOpen)
 
 # define button events, helper methods
-def fileChoose_command():
+def dirChoose_command():
 	print("File Choose!")
 
-def fileOpen_command():
+def dirOpen_command():
 	print(fileSelect_inp.get())
 
 def transfApply_command():
@@ -34,41 +36,44 @@ def save_command():
 
 # widgets
 # row 1
-fileSelect_label = tk.Label(root, text='File Selection:')
-fileSelect_label.grid(padx=1, pady=2.5, sticky='we')
+dirSelect_label = tk.Label(root, text='Directory:', anchor='e')
+dirSelect_label.grid(padx=1, pady=2.5, sticky='we')
 
-fileSelect_inp = tk.Entry(root)
-fileSelect_inp.insert(0, imagePath) # default text
-fileSelect_inp.grid(row=0, column=1, sticky='we')
+dirSelect_inp = tk.Entry(root)
+dirSelect_inp.insert(0, imagePath) # default text
+dirSelect_inp.grid(row=0, column=1, sticky='we')
 
-fileChoose_btn = tk.Button(root, text='Choose File', command=fileChoose_command)
-fileChoose_btn.grid(row=0, column=2, padx=1, pady=3, sticky='we')
+dirChoose_btn = tk.Button(root, text='Select', command=dirChoose_command)
+dirChoose_btn.grid(row=0, column=2, padx=2, pady=3, sticky='we')
 
-fileOpen_btn = tk.Button(root, text='Open File', command=fileOpen_command)
-fileOpen_btn.grid(row=0, column=3, padx=1, pady=3, sticky='we')
+dirOpen_btn = tk.Button(root, text='Open', command=dirOpen_command)
+dirOpen_btn.grid(row=0, column=3, padx=2, pady=3, sticky='we')
 
 # row 1
 transfs = ['Vertical Streak', 'Horizontal Streak', 'Boost Hue', 'Gaussian Blur']
 
-transf_label = tk.Label(root, text="Transformation:")
-transf_label.grid(row=1, column=0, padx=1, pady=3, sticky='we')
+transf_label = tk.Label(root, text='Transformation:', anchor='e')
+transf_label.grid(row=1, column=0, padx=2, pady=3, sticky='we')
 
 transf_inp = tk.Listbox(root, height=1)
 transf_inp.grid(row=1, column=1, sticky='we')
 
 transfApply_btn = tk.Button(root, text="Apply", command=transfApply_command)
-transfApply_btn.grid(row=1, column=2, padx=1, pady=3, columnspan=2, sticky='we')
+transfApply_btn.grid(row=1, column=2, padx=2, pady=3, columnspan=2, sticky='we')
 
 for transformation in transfs:
 		transf_inp.insert(tk.END, transformation)
 
 # row 2
 img_label = tk.Label(root, bg='grey', image=imagePreview)
-img_label.grid(row=2, column=0, padx=5, columnspan=4, sticky='nesw')
+img_label.grid(row=2, column=0, padx=2, columnspan=4, sticky='nesw')
 
 # row 3
+status_label = tk.Label(root, text='', anchor='e')
+status_label.grid(row = 99, column = 1, padx=2, pady = 3, sticky = "ew")
+
 save_btn = tk.Button(root, text='Save', command=save_command)
-save_btn.grid(row=99, column=1, pady=5, sticky = "sew")
+save_btn.grid(row=99, column=2, columnspan=2, padx=2, pady=3, sticky = "sew")
 
 # last line
 root.mainloop()
