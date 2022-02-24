dedb09866d24daea637b9aa6dfe312780e32ff74
Verdant808 <rapp0660@uwlax.edu>
2022-02-23 18:33:20 -0600

    Improved button functionality
    Moved each command method after the declaration of each button widget. Moved the setting of a command argument for each button widget to a separate configure call.
    
    Changed some functionality for the dirOpen_command and transfApply_command methods.
    
    The status_label widget is now updated by some methods.

--- Window.py
+++ Window.py
@@ -13,26 +13,15 @@ root.geometry('800x600')
 root.columnconfigure(1, weight=1)
 root.rowconfigure(2, weight=1)
 
-# default image
+# global variables
+# **Note that transformations should modify imageOpen,
+# which can then be used to generate a preview.**
 path = os.getcwd() + os.path.sep + 'datafiles'
 imageNames = os.listdir(path)
-imagePath = path + os.path.sep + imageNames[2]
+imagePath = path + os.path.sep + imageNames[0]
 imageOpen = Image.open(imagePath)
-imagePreview = ImageTk.PhotoImage(imageOpen)
+imagePreview = ImageTk.PhotoImage(Image.open(imagePath))
 
-# define button events, helper methods
-def dirChoose_command():
-	print("File Choose!")
-
-def dirOpen_command():
-	print(dirSelect_inp.get())
-
-def transfApply_command():
-	for i in transf_inp.curselection():
-		print(transf_inp.get(i))
-
-def save_command():
-	print("Save!")
 
 # widgets
 # row 1
@@ -43,26 +32,57 @@ dirSelect_inp = tk.Entry(root)
 dirSelect_inp.insert(0, imagePath) # default text
 dirSelect_inp.grid(row=0, column=1, sticky='we')
 
-dirChoose_btn = tk.Button(root, text='Select', command=dirChoose_command)
+dirChoose_btn = tk.Button(root, text='Select')
 dirChoose_btn.grid(row=0, column=2, padx=2, pady=3, sticky='we')
 
-dirOpen_btn = tk.Button(root, text='Open', command=dirOpen_command)
+def dirChoose_command():
+	# Opens a dialog to help select a directory
+	status_label.configure(text='Directory selected.')
+
+dirChoose_btn.configure(command=dirChoose_command)
+
+dirOpen_btn = tk.Button(root, text='Open')
 dirOpen_btn.grid(row=0, column=3, padx=2, pady=3, sticky='we')
 
-# row 1
-transfs = ['Vertical Streak', 'Horizontal Streak', 'Boost Hue', 'Gaussian Blur']
+def dirOpen_command():
+	# Opens the selected directory and loads its contents
+	# path = dirSelect_inp.get() # .replace("\\\\", os.path.sep)
+	# imageNames = os.listdir(path)
+	imagePath = path + os.path.sep + imageNames[1]
+	imageOpen = Image.open(imagePath)
+	imagePreview = ImageTk.PhotoImage(imageOpen)
+	img_label.configure(image=imagePreview)
+	img_label.image = imagePreview
 
+	status_label.configure(text='Opened a new file.')
+
+dirOpen_btn.configure(command=dirOpen_command)
+
+
+# row 1
 transf_label = tk.Label(root, text='Transformation:', anchor='e')
 transf_label.grid(row=1, column=0, padx=2, pady=3, sticky='we')
 
+transfs = ['Vertical Streak', 'Horizontal Streak', 'Hue Boost', 'Gaussian Blur']
 transf_inp = tk.Listbox(root, height=1)
 transf_inp.grid(row=1, column=1, sticky='we')
 
-transfApply_btn = tk.Button(root, text="Apply", command=transfApply_command)
-transfApply_btn.grid(row=1, column=2, padx=2, pady=3, columnspan=2, sticky='we')
-
 for transformation in transfs:
 		transf_inp.insert(tk.END, transformation)
+
+transfApply_btn = tk.Button(root, text="Apply")
+transfApply_btn.grid(row=1, column=2, padx=2, pady=3, columnspan=2, sticky='we')
+
+def transfApply_command():
+	# Applies a transformation selected via the transformation input listbox
+	selected = transf_inp.curselection()
+	if not selected:
+		status_label.configure(text='No transformation selected.')
+	else:
+		transformation = transfs[selected[0]]
+		status_label.configure(text='Applied a '+transformation+'.')
+
+transfApply_btn.configure(command=transfApply_command)
 
 # row 2
 img_label = tk.Label(root, bg='grey', image=imagePreview)
@@ -72,8 +92,14 @@ img_label.grid(row=2, column=0, padx=2, columnspan=4, sticky='nesw')
 status_label = tk.Label(root, text='', anchor='e')
 status_label.grid(row = 99, column = 1, padx=2, pady = 3, sticky = "ew")
 
-save_btn = tk.Button(root, text='Save', command=save_command)
+save_btn = tk.Button(root, text='Save')
 save_btn.grid(row=99, column=2, columnspan=2, padx=2, pady=3, sticky = "sew")
+
+def save_command():
+	# Save the modified image
+	status_label.configure(text='Image saved.')
+
+save_btn.configure(command=save_command)
 
 # last line
 root.mainloop()
