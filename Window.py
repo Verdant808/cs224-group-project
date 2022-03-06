3b79e23fd665dcb305fdc523302ae5034f240d4f
Jeremy Anderson <52007940+SnackMeUp@users.noreply.github.com>
2022-03-06 13:49:21 -0600

    -

--- Window.py
+++ Window.py
@@ -2,114 +2,135 @@
 # Built alongside: https://www.youtube.com/watch?v=LeeCrwgHYnw&list=PLXlKT56RD3kBUYQiG_jrAMOtm_SfPLvwR
 # @author Nolan Rapp
 
-from PIL import ImageTk, Image
 import tkinter as tk
-import os
+from tkinter import ttk, messagebox, Label
+from PIL import ImageTk, Image
+import os, spotify, shutil, subprocess, threading
 
 # first line
-root = tk.Tk()
-# root setup
-root.title('Title')
-root.geometry('800x600')
-root.columnconfigure(1, weight=1)
-root.rowconfigure(2, weight=1)
+window = tk.Tk()
+window.title('Top Album Glitch Art')
+window.geometry('640x700')
 
 # global variables
 # **Note that transformations should modify imageOpen,
 # which can then be used to generate a preview.**
 path = os.getcwd() + os.path.sep + 'datafiles'
+spotify.getAlbumCovers(path)
+imageList = [fname for fname in os.listdir(path)]
 imageNames = os.listdir(path)
 imagePath = path + os.path.sep + imageNames[0]
 imageOpen = Image.open(imagePath)
 imagePreview = ImageTk.PhotoImage(Image.open(imagePath))
+current_transformation = ""
 
+# Make frame fill the whole window
+topFrame = tk.Frame(window)
+topFrame.pack(fill=tk.BOTH, expand=1)
 
-# widgets
+# row 0
+tk_current_image = tk.StringVar()
+image_select_input = tk.ttk.Combobox(topFrame, values=imageList, textvariable=tk_current_image, state='readonly')
+image_select_input.set('Select an image')
+image_select_input.grid(row=0, column=0, columnspan=3, sticky='EW')
+
 # row 1
-dirSelect_label = tk.Label(root, text='Directory:', anchor='e')
-dirSelect_label.grid(padx=1, pady=2.5, sticky='we')
+tk_current_transformation = tk.StringVar()
+transformation_select_input = tk.ttk.Combobox(topFrame, values=['Lightness', 'Hue', 'Saturation', 'Intensity', 'Minimum'], textvariable=tk_current_transformation, state='readonly')
+transformation_select_input.set('Select a transformation')
+transformation_select_input.grid(row=1, column=0, columnspan=3, sticky='EW')
 
-dirSelect_inp = tk.Entry(root)
-dirSelect_inp.insert(0, imagePath) # default text
-dirSelect_inp.grid(row=0, column=1, sticky='we')
+# row 2
+topFrame.rowconfigure(2, weight=2)
+img_label = tk.Label(topFrame, bg='grey', image=imagePreview, width=640, height=640)
+img_label.grid(row=2, column=0, columnspan=3, sticky='EWNS')
 
-dirChoose_btn = tk.Button(root, text='Select')
-dirChoose_btn.grid(row=0, column=2, padx=2, pady=3, sticky='we')
+# row 3 - column 0
+topFrame.columnconfigure(0, weight=1)
+discard_btn = tk.Button(topFrame, text="Undo")
+discard_btn.grid(row=3, column=0, sticky='EW')
 
-def dirChoose_command():
-	# Opens a dialog to help select a directory
-	status_label.configure(text='Directory selected.')
+# row 3 - column 1
+topFrame.columnconfigure(1, weight=5)
+processing_bar = ttk.Progressbar( topFrame, orient='horizontal', mode='indeterminate')
+processing_bar.grid(row=3, column=1, sticky='EW')
 
-dirChoose_btn.configure(command=dirChoose_command)
+# row 3 - column 2
+topFrame.columnconfigure(2, weight=1)
+save_btn = tk.Button(topFrame, text="Save")
+save_btn.grid(row=3, column=2, sticky='EW')
 
-dirOpen_btn = tk.Button(root, text='Open')
-dirOpen_btn.grid(row=0, column=3, padx=2, pady=3, sticky='we')
-
-def dirOpen_command():
-	# Opens the selected directory and loads its contents
-	# path = dirSelect_inp.get() # .replace("\\\\", os.path.sep)
-	# imageNames = os.listdir(path)
-	imagePath = path + os.path.sep + imageNames[1]
+## Events
+# image selection
+def image_select_command(event):
+	global imagePath
+	imagePath = path + os.path.sep + tk_current_image.get()
 	imageOpen = Image.open(imagePath)
 	imagePreview = ImageTk.PhotoImage(imageOpen)
 	img_label.configure(image=imagePreview)
 	img_label.image = imagePreview
+image_select_input.bind('<<ComboboxSelected>>', image_select_command)
 
-	status_label.configure(text='Opened a new file.')
-
-dirOpen_btn.configure(command=dirOpen_command)
-
-
-# row 1
-transf_label = tk.Label(root, text='Transformation:', anchor='e')
-transf_label.grid(row=1, column=0, padx=2, pady=3, sticky='we')
-
-transfs = ['Vertical Streak', 'Horizontal Streak', 'Hue Boost', 'Gaussian Blur']
-transf_inp = tk.Listbox(root, height=1)
-transf_inp.grid(row=1, column=1, sticky='we')
-
-for transformation in transfs:
-		transf_inp.insert(tk.END, transformation)
-
-transfApply_btn = tk.Button(root, text="Apply")
-transfApply_btn.grid(row=1, column=2, padx=2, pady=3, columnspan=2, sticky='we')
-
+# transformation selection event
 def transfApply_command():
-	# Applies a transformation selected via the transformation input listbox
-	selected = transf_inp.curselection()
-	if not selected:
-		status_label.configure(text='No transformation selected.')
-	else:
-		transformation = transfs[selected[0]]
-		status_label.configure(text='Applied a '+transformation+'.')
+	match current_transformation:
+		case 'Lightness':
+			newImagePath = path + os.path.sep + 'temp.png'
+			subprocess.run(['python', '-m', 'glitchart', imagePath,'-o', newImagePath, '-s', 'lightness'])
+		case 'Hue':
+			tk.messagebox.showwarning("Warning", "Transformation not yet implemented")
+			return
+		    # newImagePath = path + os.path.sep + 'temp.png'
+		    # subprocess.run(['python', '-m', 'glitchart', imagePath, '-o', newImagePath, '-s', 'hue'])
+		case 'Saturation':
+			tk.messagebox.showwarning("Warning", "Transformation not yet implemented")
+			return			
+		    # newImagePath = path + os.path.sep + 'temp.png'
+		    # subprocess.run(['python', '-m', 'glitchart', imagePath, '-o', newImagePath, '-s', 'saturation'])
+		case 'Intensity':
+			tk.messagebox.showwarning("Warning", "Transformation not yet implemented")
+			return
+		    # newImagePath = path + os.path.sep + 'temp.png'
+		    # subprocess.run(['python', '-m', 'glitchart', imagePath, '-o', newImagePath, '-s', 'intensity'])
+		case 'Minimum':
+			tk.messagebox.showwarning("Warning", "Transformation not yet implemented")
+			return
+		    # newImagePath = path + os.path.sep + 'temp.png'
+		    # subprocess.run(['python', '-m', 'glitchart', imagePath, '-o', newImagePath, '-s', 'minimum'])
 
-transfApply_btn.configure(command=transfApply_command)
+	imageOpen = Image.open(newImagePath)
+	imagePreview = ImageTk.PhotoImage(imageOpen)
+	img_label.configure(image=imagePreview)
+	img_label.image = imagePreview
 
-# row 2
-img_label = tk.Label(root, bg='grey', image=imagePreview)
-img_label.grid(row=2, column=0, padx=2, columnspan=4, sticky='nesw')
+# transformation selection event
+def run_function():
+	processing_bar.start(interval=10)
+	transfApply_command()
+	processing_bar.stop()
 
-# row 3
-discard_btn = tk.Button(root, text='Discard Changes')
-discard_btn.grid(row=99, column=0, padx=2, pady=3, sticky='ew')
+# transformation selection event
+def transformation_select_command(event):
+	global current_transformation
+	current_transformation = tk_current_transformation.get()
+	threading.Thread(target=run_function).start()
+transformation_select_input.bind('<<ComboboxSelected>>', transformation_select_command)
 
+# discard click event
 def discard_command():
-	# Discard changes, load a fresh copy of the selected image
-	status_label.configure(text='Discarded changes.')
-
+	imageOpen = Image.open(imagePath)
+	imagePreview = ImageTk.PhotoImage(imageOpen)
+	img_label.configure(image=imagePreview)
+	img_label.image = imagePreview
 discard_btn.configure(command=discard_command)
 
-status_label = tk.Label(root, text='', anchor='e')
-status_label.grid(row = 99, column = 1, padx=2, pady=3, sticky = 'e')
+# close event
+def on_closing():
+	if tk.messagebox.askokcancel("Quit", "Do you want to quit?"):
+		imageOpen.close()
+		for file in os.listdir(path):
+			os.remove(os.path.join(path, file))
+		window.destroy()
+window.protocol("WM_DELETE_WINDOW", on_closing)
 
-save_btn = tk.Button(root, text='Save')
-save_btn.grid(row=99, column=2, columnspan=2, padx=2, pady=3, sticky = 'sew')
-
-def save_command():
-	# Save the modified image
-	status_label.configure(text='Image saved.')
-
-save_btn.configure(command=save_command)
-
-# last line
-root.mainloop()
+window.mainloop()
