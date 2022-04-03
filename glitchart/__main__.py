8861da414215f1e3ca6f99be57f77ca40ff17a83
Tanner Turba <tanner.p.turba@gmail.com>
2022-04-02 20:32:54 -0500

    Updated glitchart

--- glitchart/__main__.py
+++ glitchart/__main__.py
@@ -1,20 +1,20 @@
+from time import ctime
 from PIL import Image
 from glitchart.command_line_args import get_args
 from glitchart.main import pixelsort
 from datetime import datetime
 
-# args = get_args()
+args = get_args()
 
-def get_args(image_path, output_name, lower_threshold, upper_threshold, angle, sorting_func, interval_func):
-    args = {}
+# pop image path, open, and add to dictionary
+image_path = args.pop('image')
+args['image'] = Image.open(image_path)
 
-    # open image and add to dictionary
-    args['image'] = Image.open(image_path)
+# pop output path and create if none was entered
+output_path = args.pop('output_name')
+if output_path is None: 
+    date = datetime.today()
+    output_path = f'{date.hour}:{date.minute}:{date.second} {date.month}-{date.day}-{date.year}.png'
 
-    # pop output path and create if none was entered
-    output_path = args.pop('output_name')
-    if output_path is None: 
-        output_path = 'glitched_out.png'
-
-    # perform the pixelsorting and save image to pathname
-    pixelsort(**args).save(output_path)
+# perform the pixelsorting and save image to pathname
+pixelsort(**args).save(output_path)
--- glitchart/interval_creator.py
+++ glitchart/interval_creator.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from glitchart.conversion import lightness
+import random
 
 # creates and returns a list of intervals for sorting, based on lightness
 def threshold(image, lower_threshold, upper_threshold, **args):
@@ -17,6 +18,41 @@ def threshold(image, lower_threshold, upper_threshold, **args):
                 intervals[y].append(x)
     return intervals
 
+# TODO: DOESN'T WORK YET
+# creates an interval and adds all pixels to for sorting 
+def none(image, **args):
+    intervals = []
+    image_data = image.load()
+
+    # for each column of pixels
+    for y in range(image.size[1]):
+        intervals.append([])
+        # get each individual pixel, row by row
+        for x in range(image.size[0]):
+            # add pixel to interval
+            intervals[y].append(x)
+    return intervals
+
+
+# creates an interval and adds pixels at random for sorting
+def random_intervals(image, lower_threshold, upper_threshold, **args):
+    intervals = []
+    image_data = image.load()
+
+    # for each column of pixels
+    for y in range(image.size[1]):
+        intervals.append([])
+        # get each individual pixel, row by row
+        for x in range(image.size[0]):
+            # add pixel to interval at random
+            rand = random.random()
+            if rand < lower_threshold or rand > upper_threshold:
+                intervals[y].append(x)
+    return intervals
+
+
 CHOICES = {
-    'threshold': threshold
+    'threshold': threshold,
+    'none': none,
+    'random': random_intervals
 }
