09524fe206ba0e5d3d5c89d57d6e0ab63d6b5b9f
Jeremy Anderson <52007940+SnackMeUp@users.noreply.github.com>
2022-05-01 11:31:23 -0500

    Spotify updates
    Removed commented out code and updated code to remove special characters from filename.

--- spotify.py
+++ spotify.py
@@ -3,6 +3,7 @@
 # SPOTIPY MANUAL: https://spotipy.readthedocs.io/en/2.19.0/#module-spotipy.oauth2
 
 import os
+import re
 import spotipy
 import urllib.request
 
@@ -15,9 +16,6 @@ def getAlbumCovers(filepath):
                                                         redirect_uri="http://localhost:8080/callback",
                                                         scope=scope))
 
-    # Get the current user's profile information and their top x tracks.
-    # user = spotify.me()
-
     # Create a temp spotify directory if it doesn't already exist.
     if not os.path.exists(filepath):
         os.makedirs(filepath)
@@ -25,9 +23,9 @@ def getAlbumCovers(filepath):
     # Get the current users top 25 tracks and download the album art for each.
     top_tracks = spotify.current_user_top_tracks(time_range='long_term', limit=25)
     for i, item in enumerate(top_tracks['items']):
-        urllib.request.urlretrieve(item['album']['images'][0]['url'], filepath + (os.path.sep + item['album']['name']).replace(" ", "") + ".jpeg")
+        urllib.request.urlretrieve(item['album']['images'][0]['url'], filepath + (os.path.sep + re.sub('[^A-Za-z0-9]+', '', item['album']['name'])) + ".jpeg")
 
     # Get the current users playlist artwork
     result = spotify.current_user_playlists(limit=50, offset=0)
     for i, item in enumerate(result['items']):
-        urllib.request.urlretrieve(item['images'][0]['url'], filepath + (os.path.sep + (item['name'].replace("/", "")).replace(" ", "")) + ".jpeg")
+        urllib.request.urlretrieve(item['images'][0]['url'], filepath + (os.path.sep + re.sub('[^A-Za-z0-9]+', '', item['name'])) + ".jpeg")
